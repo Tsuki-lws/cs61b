11acda8824c830b469f8b3acb94444fb96141e96
Luo wusheng <luowusheng7@gmail.com>
2024-08-01 22:14:44 +0800

    hw0b: Completed all

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,7 +6,7 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1,2,3,4,5,6};
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +15,30 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if(customer.equals("Ergun")){
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        } else if (customer.equals("Erik")) {
+            return new String[]{"sushi", "pasta", "avocado", "coffee"};
+        }else{
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int j : array) {
+            if (j > max) {
+                max = j;
+            }
+            if (j < min) {
+                min = j;
+            }
+        }
+        return max - min;
     }
 
     /**
@@ -39,7 +55,18 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if(x % 2 == 0){
+            x /= 2;
+        }else{
+            x = 3 * x + 1;
+        }
+        if(x == 1){
+            list.add(x);
+            return list;
+        }else{
+           return hailstoneHelper(x,list);
+        }
     }
 
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,11 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> characterIntegerMap = new HashMap<>();
+        for(char i = 'a';i <='z';i++){
+            characterIntegerMap.put(i,i-'a'+1);
+        }
+        return characterIntegerMap;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +20,30 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        if(nums.isEmpty()){
+            return null;
+        }
+        Map<Integer,Integer> a = new HashMap<>();
+        for(int i : nums){
+            a.put(i,i*i);
+        }
+        return a;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        if(words.isEmpty()){
+            return null;
+        }
+        Map<String,Integer> a = new HashMap<>();
+        for(String b:words){
+            if(a.containsKey(b)){
+                a.put(b,a.get(b)+1);
+            }else{
+                a.put(b,1);
+            }
+        }
+        return a;
     }
 }
