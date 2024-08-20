7b389677344ad92d66dae0d9d3f418ac1bae0b4b
Luo wusheng <luowusheng7@gmail.com>
2024-08-20 15:26:54 +0800

    Complete lab3

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -91,6 +91,8 @@ public class SpeciesListStage implements AdventureStage {
      * If there is no similarity, it should return 0.
      */
     public static int arraySimilarity(List<String> listOne, List<String> listTwo) {
+        if(listOne.isEmpty())
+            return 0;
         List<String> copy = new ArrayList<>(listOne);
         int similarObjects = 0;
         for (String o : listTwo) {
@@ -99,6 +101,7 @@ public class SpeciesListStage implements AdventureStage {
                 copy.remove(o);
             }
         }
+
         return similarObjects / listOne.size();
     }
 }
